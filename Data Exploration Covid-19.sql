52d623bf0e67e4750725eb1e750c128963d3a09f
Kittipod Tanapivattanakul <116799751+jeremyfight@users.noreply.github.com>
2023-12-03 22:02:06 +0700

    Create Data Exploration Covid-19.sql

---
+++ Data Exploration Covid-19.sql
@@ -0,0 +1,30 @@
+-- Select Data that we are going to be starting with
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From CovidDeath
+Where continent is not null 
+order by 1,2
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From CovidDeath
+Where location like '%states%'
+and continent is not null 
+order by 1,2
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+order by 1,2
+
+  -- Countries with Highest Infection Rate compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeath
+Group by Location, Population
+order by PercentPopulationInfected desc
